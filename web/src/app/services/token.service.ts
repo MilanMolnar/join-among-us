7e995e8fb980faf738512894a3a23999d8de975a
Viktor Ferenczi <ferencziviktor@gmail.com>
2020-10-15 14:12:50 +0200

    cors error 7:58

--- web/src/app/services/token.service.ts
+++ web/src/app/services/token.service.ts
@@ -3,18 +3,40 @@ import { ErrorHandler, Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
+
 export class TokenService {
 
 constructor() { }
 
 
-handle(token){
-  this.set(token);
-}
+  handle(token){
+    this.set(token);
+    console.log(this.payload(token));
+  }
 
-set(token){
-  localStorage.setItem('token', token);
-}
+  set(token){
+    localStorage.setItem('token', token);
+  }
+
+  get() {
+    return localStorage.getItem('token');
+  }
+
+  remove() {
+    localStorage.removeItem('token');
+  }
+
+  isValid() { 
+    const token = this.get();
+    if(token) {
+      const payload = this.payload(token);
+    
+    }
+  }
+
+  payload(token) {
+    return token.split('.')[1];
+  }
 
 }
 
